68fec431c937c2b4400944758c4892a85cedfd96
Anthony POLA <anthonypola100@gmail.com>
2021-06-07 23:53:02 +0100

    first commit

---
+++ src/test/java/com/example/IntegrationContinue/IntegrationContinueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrationContinue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationContinueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
